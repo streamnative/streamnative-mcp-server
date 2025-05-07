51d3107f83841f34cd0758e25efc1da90e50ea86
Rui Fu <freeznet@users.noreply.github.com>
2025-05-08 00:35:02 +0800

    fix: cleanup (#8)
    - fix license
    - ready to export as lib

--- pkg/mcp/context_utils.go
+++ pkg/mcp/context_utils.go
@@ -1,12 +1,12 @@
-// Licensed to Elasticsearch B.V. under one or more contributor
-// license agreements. See the NOTICE file distributed with
-// this work for additional information regarding copyright
-// ownership. Elasticsearch B.V. licenses this file to you under
-// the Apache License, Version 2.0 (the "License"); you may
-// not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//   http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing,
 // software distributed under the License is distributed on an
@@ -61,7 +61,7 @@ func SetContext(options *config.Options, instanceName, clusterName string) error
 		}
 	}
 	if !foundInstance {
-		return fmt.Errorf("Pulsar instance %s not found", instanceName)
+		return fmt.Errorf("Pulsar instance %s not found in organization %s", instanceName, options.Organization)
 	}
 
 	clusters, clustersBody, err := apiClient.CloudStreamnativeIoV1alpha1Api.ListCloudStreamnativeIoV1alpha1NamespacedPulsarCluster(ctx, options.Organization).Execute()
