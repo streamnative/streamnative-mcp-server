dfa57d1f6170e19daab34b08e2bcb9364ed2f09d
Sijie Guo <sg@streamnative.io>
2025-05-11 15:41:05 -0700

    Refactor the mcp server

--- pkg/cmd/mcp/stdio.go
+++ pkg/cmd/mcp/stdio.go
@@ -65,7 +65,7 @@ func runStdioServer(configOpts *ServerOptions) error {
 	// Create a new MCP server
 	ctx = context.WithValue(ctx, mcp.OptionsKey, configOpts.Options)
 	stdLogger := stdlog.New(logger.Writer(), "snmcp-server", 0)
-	stdioServer := server.NewStdioServer(newStdioServer(configOpts, logger))
+	stdioServer := server.NewStdioServer(newMcpServer(configOpts, logger))
 
 	stdioServer.SetErrorLogger(stdLogger)
 
@@ -119,83 +119,4 @@ func initLogger(filePath string) (*logrus.Logger, error) {
 	logger.SetLevel(logrus.DebugLevel)
 	logger.SetOutput(fd)
 	return logger, nil
-}
-
-func newStdioServer(configOpts *ServerOptions, logrusLogger *logrus.Logger) *server.MCPServer {
-	snConfig := configOpts.Options.LoadConfigOrDie()
-	var s *server.MCPServer
-	switch {
-	case snConfig.KeyFile != "":
-		{
-			issuer := snConfig.Auth.Issuer()
-			userName, err := configOpts.Options.WhoAmI(issuer.Audience)
-			if err != nil {
-				stdlog.Fatalf("failed to get user name: %v", err)
-				os.Exit(1)
-			}
-			// Create a new MCP server
-			s = server.NewMCPServer(
-				"streamnative-mcp-server",
-				"0.0.1",
-				server.WithResourceCapabilities(true, true),
-				server.WithInstructions(mcp.GetStreamNativeCloudServerInstructions(userName)),
-				server.WithLogging())
-
-			mcp.RegisterPrompts(s)
-			mcp.RegisterContextTools(s, configOpts.Features)
-			mcp.StreamNativeAddLogTools(s, configOpts.ReadOnly, configOpts.Features)
-		}
-	case snConfig.ExternalKafka != nil:
-		{
-			s = server.NewMCPServer(
-				"streamnative-mcp-server/kafka",
-				"0.0.1",
-				server.WithResourceCapabilities(true, true),
-				server.WithInstructions(mcp.GetExternalKafkaServerInstructions(snConfig.ExternalKafka.BootstrapServers)),
-				server.WithLogging())
-		}
-	case snConfig.ExternalPulsar != nil:
-		{
-			s = server.NewMCPServer(
-				"streamnative-mcp-server/pulsar",
-				"0.0.1",
-				server.WithResourceCapabilities(true, true),
-				server.WithInstructions(mcp.GetExternalPulsarServerInstructions(snConfig.ExternalPulsar.WebServiceURL)),
-				server.WithLogging())
-		}
-	default:
-		{
-			stdlog.Fatalf("no valid configuration found")
-			os.Exit(1)
-		}
-	}
-
-	mcp.PulsarAdminAddBrokersTools(s, configOpts.ReadOnly, configOpts.Features)
-	mcp.PulsarAdminAddBrokerStatsTools(s, configOpts.ReadOnly, configOpts.Features)
-	mcp.PulsarAdminAddClusterTools(s, configOpts.ReadOnly, configOpts.Features)
-	mcp.PulsarAdminAddFunctionsWorkerTools(s, configOpts.ReadOnly, configOpts.Features)
-	mcp.PulsarAdminAddNamespaceTools(s, configOpts.ReadOnly, configOpts.Features)
-	mcp.PulsarAdminAddNamespacePolicyTools(s, configOpts.ReadOnly, configOpts.Features)
-	mcp.PulsarAdminAddNsIsolationPolicyTools(s, configOpts.ReadOnly, configOpts.Features)
-	mcp.PulsarAdminAddPackagesTools(s, configOpts.ReadOnly, configOpts.Features)
-	mcp.PulsarAdminAddResourceQuotasTools(s, configOpts.ReadOnly, configOpts.Features)
-	mcp.PulsarAdminAddSchemasTools(s, configOpts.ReadOnly, configOpts.Features)
-	mcp.PulsarAdminAddSubscriptionTools(s, configOpts.ReadOnly, configOpts.Features)
-	mcp.PulsarAdminAddTenantTools(s, configOpts.ReadOnly, configOpts.Features)
-	mcp.PulsarAdminAddTopicTools(s, configOpts.ReadOnly, configOpts.Features)
-	mcp.PulsarAdminAddSinksTools(s, configOpts.ReadOnly, configOpts.Features)
-	mcp.PulsarAdminAddFunctionsTools(s, configOpts.ReadOnly, configOpts.Features)
-	mcp.PulsarAdminAddSourcesTools(s, configOpts.ReadOnly, configOpts.Features)
-	mcp.PulsarAdminAddTopicPolicyTools(s, configOpts.ReadOnly, configOpts.Features)
-	mcp.PulsarClientAddConsumerTools(s, configOpts.ReadOnly, configOpts.Features)
-	mcp.PulsarClientAddProducerTools(s, configOpts.ReadOnly, configOpts.Features)
-
-	mcp.KafkaAdminAddTopicTools(s, configOpts.ReadOnly, configOpts.Features)
-	mcp.KafkaAdminAddPartitionsTools(s, configOpts.ReadOnly, configOpts.Features)
-	mcp.KafkaAdminAddGroupsTools(s, configOpts.ReadOnly, configOpts.Features)
-	mcp.KafkaAdminAddSchemaRegistryTools(s, configOpts.ReadOnly, configOpts.Features)
-	mcp.KafkaAdminAddKafkaConnectTools(s, configOpts.ReadOnly, configOpts.Features)
-	mcp.KafkaClientAddConsumeTools(s, configOpts.ReadOnly, logrusLogger, configOpts.Features)
-	mcp.KafkaClientAddProduceTools(s, configOpts.ReadOnly, configOpts.Features)
-	return s
 }
